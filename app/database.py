c3baa120366930899779797b6f5b36b353a2ea21
randhawa2132 <77087724+randhawa2132@users.noreply.github.com>
2025-11-09 22:36:01 -0700

    Retry login query after initializing the database

--- app/database.py
+++ app/database.py
@@ -1,20 +1,23 @@
 from __future__ import annotations
 
-from contextlib import contextmanager
-from typing import Iterator
+from collections.abc import Generator
 
+from sqlalchemy.orm import sessionmaker
 from sqlmodel import Session, SQLModel, create_engine
 
 from .settings import settings
 
 engine = create_engine(str(settings.database_url), echo=settings.debug)
+SessionLocal = sessionmaker(bind=engine, class_=Session, expire_on_commit=False)
 
 
 def init_db() -> None:
     SQLModel.metadata.create_all(engine)
 
 
-@contextmanager
-def get_session() -> Iterator[Session]:
-    with Session(engine) as session:
+def get_session() -> Generator[Session, None, None]:
+    session = SessionLocal()
+    try:
         yield session
+    finally:
+        session.close()
